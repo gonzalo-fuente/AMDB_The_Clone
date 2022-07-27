414507c6062482fda56f0d27e074c6d1dea573c2
gonzalo <gonzalo_a_fuente@hotmail.com>
2022-07-27 20:21:09 -0300

    Feat: User registration implemented

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosToken = axios.create({
-  baseURL: "http://challenge-react.alkemy.org",
+  baseURL: process.env.REACT_APP_API_ENDPOINT,
 });
 
 export const axiosTMDB = axios.create({
